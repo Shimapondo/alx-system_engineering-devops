3c08ed78af4c69e8683c6faece09307ba0539364
Shimapondo <williamnamabende@gmail.com>
2023-12-27 13:38:35 -0800

    correctins on installing a flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-# install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem'
+# module that helps install flask onto the machine
+# the version is 2.1.0 from pip3
+
+exec { 'flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin']
 }
+
+#install the needed Werkzeug==2.1.1 for compatibility
+exec { 'Werkzeug':
+  command => 'pip3 install --upgrade Werkzeug==2.1.1',
+  path    => ['/usr/bin', '/bin']
+}
+
+
